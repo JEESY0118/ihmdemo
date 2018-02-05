8beb16455b84e68de9ffc5b02a106eab31bfddf9
Sami Lazrak <sami.lazrak@etu.unice.fr>
2018-02-05 16:49:08 +0100

    Creer le gitignore et le controller de FenetreAcceuil

---
+++ src/main/java/fr/polytech/ihm/FenetreAcceuilController.java
@@ -0,0 +1,12 @@
+package fr.polytech.ihm;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class FenetreAcceuilController {
+
+    @FXML
+    private Button declare;
+
+}
