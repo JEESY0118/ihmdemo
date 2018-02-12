d781d18c21c29245bbef1b229bf14b7f4a74cc0c
Sami Lazrak <sami.lazrak@etu.unice.fr>
2018-02-12 17:30:16 +0100

    creer et relier button dans historique incidents

--- src/main/java/fr/polytech/ihm/controllers/HistoriqueIncidentsController.java
+++ src/main/java/fr/polytech/ihm/controllers/HistoriqueIncidentsController.java
@@ -1,4 +1,19 @@
 package fr.polytech.ihm.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TitledPane;
+
 public class HistoriqueIncidentsController extends Controllers {
+    @FXML
+    private TitledPane titledPane;
+
+    @FXML
+    private Button retour;
+
+    @FXML
+    public void handleRetourButton(){
+        String fxmlFile = "/fxml/FenetreAcceuil.fxml";
+        showScene(fxmlFile, titledPane,"Fenetre Acceuil");
+    }
 }
