8666e9f6d27a75bce0a366f1708723fc2970fd80
Wei WANG <wei.wang@etu.unice.fr>
2018-02-12 15:48:15 +0100

    add action in DeclarationIncidentController

--- src/main/java/fr/polytech/ihm/DeclarationIncidentController.java
+++ src/main/java/fr/polytech/ihm/DeclarationIncidentController.java
@@ -1,4 +1,34 @@
 package fr.polytech.ihm;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TitledPane;
+
+
 public class DeclarationIncidentController extends Controllers{
+
+        @FXML
+        private Button annuler;
+
+        @FXML
+        private Button ajouter;
+
+        @FXML
+        private TitledPane titledPane;
+
+        @FXML
+        void handleAnnulerButton() {
+            String fxmlFile = "/fxml/FenetreAcceuil.fxml";
+            showScene(fxmlFile, titledPane,"Fenetre Acceuil");
+
+        }
+
+        @FXML
+        void handleAjouterButton() {
+            String fxmlFile = "/fxml/ConfirmationDeclarationIncident.fxml";
+            showScene(fxmlFile, titledPane,"Confirmation Declaration Incident");
+        }
+
+
+
 }
